65ce7694ae8a4be27be20cbab3cb71c1763983d3
Damien Morcellet <dmorcellet@free.fr>
2024-04-11 14:24:42 +0200

    Added HTML constants

---
+++ src/main/java/delta/common/utils/html/HtmlConstants.java
@@ -0,0 +1,25 @@
+package delta.common.utils.html;
+
+/**
+ * Some HTML constants.
+ * @author DAM
+ */
+public class HtmlConstants
+{
+  /**
+   * Start a bold section.
+   */
+  public static final String START_BOLD="<b>";
+  /**
+   * End a bold section.
+   */
+  public static final String END_BOLD="</b>";
+  /**
+   * Start a paragraph.
+   */
+  public static final String START_PARAGRAPH="<p>";
+  /**
+   * End a paragraph.
+   */
+  public static final String END_PARAGRAPH="</p>";
+}
