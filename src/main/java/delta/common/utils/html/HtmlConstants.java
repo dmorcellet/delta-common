81f8b6f7511aa45aa202b8eecd2b1e3caafde1ac
Damien Morcellet <dmorcellet@free.fr>
2024-05-06 22:02:16 +0200

    More HTML constants

--- src/main/java/delta/common/utils/html/HtmlConstants.java
+++ src/main/java/delta/common/utils/html/HtmlConstants.java
@@ -22,4 +22,28 @@ public class HtmlConstants
    * End a paragraph.
    */
   public static final String END_PARAGRAPH="</p>";
+  /**
+   * DIV.
+   */
+  public static final String DIV="<div>";
+  /**
+   * End of DIV.
+   */
+  public static final String END_DIV="</div>";
+  /**
+   * UL.
+   */
+  public static final String UL="<ul>";
+  /**
+   * End of UL.
+   */
+  public static final String END_UL="</ul>";
+  /**
+   * LI.
+   */
+  public static final String LI="<li>";
+  /**
+   * End of LI.
+   */
+  public static final String END_LI="</li>";
 }
